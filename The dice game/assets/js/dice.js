108a006b59fb20529ad462f6af7ab722a2f6d24c
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2020-01-24 21:28:08 +0530

    Delete dice.js

--- The dice game/assets/js/dice.js
+++
@@ -1,8 +0,0 @@
-var player1 = document.querySelector("#p1");
-var player2 = document.querySelector("#p2");
-var hold = document.querySelector("#hold");
-hold.addEventListener("click",function(){
-    player1.classList.toggle(".active");
-    player2.classList.toggle(".active");
-    alert("btn pressed");
-});
